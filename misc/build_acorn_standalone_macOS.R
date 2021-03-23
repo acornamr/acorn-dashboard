a38e42f1baf1dae4ce6e30395c8310b10b4880b0
Olivier <olivier.celhay@gmail.com>
2021-03-23 16:04:27 -0400

    Update build_acorn_standalone_macOS.R

--- misc/build_acorn_standalone_macOS.R
+++ misc/build_acorn_standalone_macOS.R
@@ -5,7 +5,7 @@ if(FALSE) {
 rm(list = ls())
 remove.packages("shinybox")
 detach("package:shinybox", unload = TRUE)
-remotes::install_github("ocelhay/shinybox", auth_token = "")
+remotes::install_github("ocelhay/shinybox")
 }
 
 # Check that the package is working
@@ -52,3 +52,27 @@ shinybox(
 run_build_release(nodejs_path = nodejs_path,
                   app_path = file.path(build_path, "ACORN"),
                   nodejs_version = nodejs_version)
+
+# with latest shinybox
+# Create a directory for the app
+time <- format(Sys.time(), "%Y-%m-%d_%H%M")
+(build_path <- paste0("/Users/olivier/Documents/Projets/ACORN/acorn-standalone_", time))
+dir.create(build_path)
+
+library(shinybox)
+shinybox(
+  app_name = "ACORN",
+  author = "Olivier Celhay, Paul Turner",
+  description = "A Dashboard for ACORN AMR Data",
+  semantic_version = "v0.0.1", # format vx.y.z
+  cran_like_url = "https://cran.microsoft.com/snapshot/2021-02-11",
+  mac_file = "/Users/olivier/Documents/Projets/Standalone R Shiny/R/macOS/2021-02-11/R-4.0-branch.tar.gz",
+  git_host = "github",
+  git_repo = "acornamr/acorn-dashboard",
+  function_name = "run_app", 
+  local_package_path = NULL,
+  package_install_opts = list(type = "binary"),
+  build_path = build_path,
+  rtools_path_win = NULL,
+  nodejs_path = "/usr/local/bin/",
+  run_build = TRUE)
