3fb87e9ba5c6e14d80509c070d4e6b9abdbaeee1
Olivier <olivier.celhay@gmail.com>
2021-07-23 17:23:52 +0200

    make dependent to contaminant filter

--- inst/acorn/www/R/outputs/tab_microbiology/isolates_growth_gauge.R
+++ inst/acorn/www/R/outputs/tab_microbiology/isolates_growth_gauge.R
@@ -1,14 +1,22 @@
 output$isolates_growth_gauge <- renderGauge({
     req(acorn_dta_filter())
     req(nrow(acorn_dta_filter()) > 0)
+    
+    ifelse(input$filter_rm_contaminant, {
+      dta <- acorn_dta_filter() %>% 
+        replace_na(list(contaminant = "No")) %>% filter(contaminant == "No")
+    }, 
+    {
+      dta <- acorn_dta_filter() 
+    })
   
-  n <- acorn_dta_filter() %>%
+  n <- dta %>%
     fun_filter_growth_only() %>%
     fun_deduplication(method = input$deduplication_method) %>%
     pull(specid) %>% 
     n_distinct()
   
-  total <- acorn_dta_filter() %>%
+  total <- dta %>%
     fun_filter_cultured_only() %>%
     fun_deduplication(method = input$deduplication_method) %>%
     pull(specid) %>% 
