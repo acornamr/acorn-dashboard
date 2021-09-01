d43a4a8b3393a2e20f355da4c0287bd9135f2c2c
Olivier <olivier.celhay@gmail.com>
2021-09-01 17:44:59 -0400

    versioning 0.15

--- inst/acorn/www/R/startup.R
+++ inst/acorn/www/R/startup.R
@@ -1,4 +1,4 @@
-app_version <- "0.0.14"  # Make sure that the app version is identical in DESCRIPTION
+app_version <- "0.0.15"  # Make sure that the app version is identical in DESCRIPTION
 
 # IMPORTANT: packages listed here should be identical in run_app.R and DESCRIPTION
 library(aws.s3)
