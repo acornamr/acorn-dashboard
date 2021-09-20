53a9c6e4e39629e95fc90f0903eef36338221c4d
Olivier <olivier.celhay@gmail.com>
2021-09-20 15:48:39 -0400

    remove logo demo

--- inst/acorn/www/R/outputs/site_logo.R
+++ inst/acorn/www/R/outputs/site_logo.R
@@ -1,5 +1,6 @@
 output$site_logo <- renderUI({
   if(is_empty(acorn_cred()$site))  return()
+  if(acorn_cred()$site == "demo")  return()
   
   return(img(src = glue("./images/logo_{acorn_cred()$site}.png"), alt = "Institution Logo", id = "logo-img"))
 })
