56c17e36276b60dd30b9a6642d3589a8803e8cef
Olivier <olivier.celhay@gmail.com>
2021-09-27 16:57:09 -0400

    Update checklist_app_deployment.R

--- misc/checklist_app_deployment.R
+++ misc/checklist_app_deployment.R
@@ -1,6 +1,17 @@
 # run once:
 # renv::init()
 
-# before deploying
+# deploy on dev URL
+rsconnect::deployApp(appDir = "./inst/acorn/", 
+                     appName = "acorn2-dev", 
+                     account = "moru")
+
+
+# if it works:
+# take a snapshot of installated package
 renv::snapshot()
 
+# deploy on production URL
+rsconnect::deployApp(appDir = "./inst/acorn/", 
+                     appName = "acorn2",
+                     account = "moru")
