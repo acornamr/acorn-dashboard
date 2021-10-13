a80e6356ccda174157dada2f2d07dc4e279aea46
Olivier <olivier.celhay@gmail.com>
2021-10-13 12:31:05 -0400

    Added space after icon.

--- inst/acorn/www/R/outputs/app_github_versions.R
+++ inst/acorn/www/R/outputs/app_github_versions.R
@@ -12,11 +12,11 @@ output$app_github_versions <- renderText({
   
   v_app_version <- glue("v{app_version}")
   
-  if(exists("version_github") & version_github == v_app_version) return(paste0("<div class='alert alert-success'><i class='fa fa-check'></i>", 
+  if(exists("version_github") & version_github == v_app_version) return(paste0("<div class='alert alert-success'><i class='fa fa-check'></i>&nbsp;", 
                                                                              i18n$t("Your ACORN Dashboard is up to date."), 
                                                                              "</div>"))
 
-  if(exists("version_github") & version_github != v_app_version) return(paste0("<div class='alert alert-warning'><i class='fa fa-exclamation-triangle'></i>", 
+  if(exists("version_github") & version_github != v_app_version) return(paste0("<div class='alert alert-warning'><i class='fa fa-exclamation-triangle'></i>&nbsp;", 
                                                                              i18n$t("You are running ACORN dashboard"),
                                                                              " ", v_app_version, " ",
                                                                              "<a href = https://github.com/acornamr/acorn-dashboard/releases/latest target='_blank' class = 'alert-link'>",
