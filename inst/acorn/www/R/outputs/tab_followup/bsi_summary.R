83578bce07e6879258801b2a602170da94b1f9ea
Olivier <olivier.celhay@gmail.com>
2021-11-05 14:21:46 -0400

    Fix BSI formulas.

--- inst/acorn/www/R/outputs/tab_followup/bsi_summary.R
+++ inst/acorn/www/R/outputs/tab_followup/bsi_summary.R
@@ -2,8 +2,8 @@ output$bsi_summary <- renderText({
   req(redcap_f01f05_dta_filter())
   req(nrow(redcap_f01f05_dta_filter()) > 0)
 
-  ecoli_bsi <- acorn_dta_filter() %>% fun_filter_blood_only() %>% filter(bsi_pahtogen == "ECOLI") %>% pull(redcap_id) %>% unique()
-  staph_bsi <- acorn_dta_filter() %>% fun_filter_blood_only() %>% filter(bsi_pahtogen == "STAPH") %>% pull(redcap_id) %>% unique()
+  ecoli_bsi <- acorn_dta_filter() %>% fun_filter_blood_only() %>% filter(bsi_pahtogen == "ECOLI") %>% pull(redcap_id) %>% n_distinct()
+  staph_bsi <- acorn_dta_filter() %>% fun_filter_blood_only() %>% filter(bsi_pahtogen == "STAPH") %>% pull(redcap_id) %>% n_distinct()
   
   ecoli_lab <- acorn_dta_filter() %>% fun_filter_blood_only() %>% filter(orgname == "Escherichia coli") %>% nrow()
   staph_lab <- acorn_dta_filter() %>% fun_filter_blood_only() %>% filter(orgname == "Staphylococcus aureus") %>% nrow()
