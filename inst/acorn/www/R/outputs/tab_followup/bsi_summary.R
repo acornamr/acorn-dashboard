f907e21310a9b4999308a2a783d687fef218bf2f
Olivier <olivier.celhay@gmail.com>
2021-11-05 12:02:31 -0400

    Correct BSI summary numbers.

--- inst/acorn/www/R/outputs/tab_followup/bsi_summary.R
+++ inst/acorn/www/R/outputs/tab_followup/bsi_summary.R
@@ -1,11 +1,12 @@
 output$bsi_summary <- renderText({
   req(redcap_f01f05_dta_filter())
   req(nrow(redcap_f01f05_dta_filter()) > 0)
+
+  ecoli_bsi <- acorn_dta_filter() %>% fun_filter_blood_only() %>% filter(bsi_pahtogen == "ECOLI") %>% pull(redcap_id) %>% unique()
+  staph_bsi <- acorn_dta_filter() %>% fun_filter_blood_only() %>% filter(bsi_pahtogen == "STAPH") %>% pull(redcap_id) %>% unique()
   
-  ecoli_bsi <- acorn_dta_filter() %>% filter(bsi_pahtogen == "ECOLI") %>% nrow()
-  staph_bsi <- acorn_dta_filter() %>% filter(bsi_pahtogen == "STAPH") %>% nrow()
-  ecoli_lab <- acorn_dta_filter() %>% filter(orgname == "Escherichia coli") %>% nrow()
-  staph_lab <- acorn_dta_filter() %>% filter(orgname == "Staphylococcus aureus") %>% nrow()
+  ecoli_lab <- acorn_dta_filter() %>% fun_filter_blood_only() %>% filter(orgname == "Escherichia coli") %>% nrow()
+  staph_lab <- acorn_dta_filter() %>% fun_filter_blood_only() %>% filter(orgname == "Staphylococcus aureus") %>% nrow()
   
   
   glue("{i18n$t('Records in Lab data and BSI forms:')}</br></br>
