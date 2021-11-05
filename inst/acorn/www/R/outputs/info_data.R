2196a502733e8bdf1d250f4fb225b55b111ca0c5
Olivier <olivier.celhay@gmail.com>
2021-11-05 12:03:02 -0400

    Improve format of date.

--- inst/acorn/www/R/outputs/info_data.R
+++ inst/acorn/www/R/outputs/info_data.R
@@ -1,7 +1,10 @@
 output$info_data <- renderText({
+  
   about_data <- ifelse(is_empty(meta()),
                        "No data available at this moment.",
-                       glue("Data generated on the {meta()$time_generation} with the following: 
+                       {
+                         time_generation <- meta()$time_generation |> as_datetime(format = "%Y-%m-%d_%HH%M") |> format("%d %B %Y at %H:%M")
+                         glue("Data generated on the {time_generation} (local time) with: 
                             <ul>
                             <li>Used dashboard version: {meta()$app_version}</li>
                             <li>Site: {meta()$site}</li>
@@ -11,7 +14,8 @@ output$info_data <- renderText({
                             Download loaded data in 
                             {downloadLink('download_data_acorn_format', 'acorn')} or
                             {downloadLink('download_data_excel_format', 'Excel')} format. 
-                            Or start exploring the data (Tabs Overview, Follow-up...)."))
+                            Or start exploring the data (Tabs Overview, Follow-up...).")
+                       })
   
   paste(HTML(about_data))
 })
