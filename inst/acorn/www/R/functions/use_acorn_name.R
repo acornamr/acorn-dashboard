42a5cb40df139a23f060067942e5f33ef4025211
Olivier <olivier.celhay@gmail.com>
2022-02-09 12:03:46 -0500

    Error when duplicated names in the data dictionary

--- inst/acorn/www/R/functions/use_acorn_name.R
+++ inst/acorn/www/R/functions/use_acorn_name.R
@@ -1,6 +1,10 @@
 # rename with data dictionary variables
 use_acorn_name <- function(x, acorn_names, local_names) {
-  if(x %in% local_names)  return(acorn_names[which(local_names == x)])
+  if(x %in% local_names) {
+    name_index <- which(local_names == x)
+    if (length(name_index) > 1)  stop("Duplicated names in the data dictionary.")
+    return(acorn_names[name_index])
+  }
   if(x %in% acorn_names)  return(x)
   return(paste0("NOT_ACORN_COLUMN_", x))
 }
