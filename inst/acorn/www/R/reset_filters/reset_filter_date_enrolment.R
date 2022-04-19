811f41e695e00ba54e86bc38e6f3f040a7f0af93
Olivier <olivier.celhay@gmail.com>
2022-04-19 10:38:08 -0400

    Fix bug date_episode_enrolment NA

--- inst/acorn/www/R/reset_filters/reset_filter_date_enrolment.R
+++ inst/acorn/www/R/reset_filters/reset_filter_date_enrolment.R
@@ -1,3 +1,3 @@
 updateDateRangeInput(session, "filter_date_enrolment",
-                     start = min(redcap_f01f05_dta()$date_episode_enrolment),
-                     end = max(redcap_f01f05_dta()$date_episode_enrolment))
+                     start = min(redcap_f01f05_dta()$date_episode_enrolment, na.rm = TRUE),
+                     end = max(redcap_f01f05_dta()$date_episode_enrolment, na.rm = TRUE))
