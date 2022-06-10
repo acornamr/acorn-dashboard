2937bc2aa712bfd09effae3ce18fd5d6f3005965
Olivier <olivier.celhay@gmail.com>
2022-06-09 21:17:02 -0400

    Fix case when v.App > v.GitHub

--- inst/acorn/www/R/outputs/app_github_versions.R
+++ inst/acorn/www/R/outputs/app_github_versions.R
@@ -5,21 +5,28 @@ output$app_github_versions <- renderText({
     version_github <- rvest::read_html("https://github.com/acornamr/acorn-dashboard/releases/latest") |> 
       rvest::html_element("h1.d-inline") |>
       rvest::html_text() |>
-      str_trim(),
+      str_trim() |> 
+      sub(pattern = "v", replacement = ""),
     silent = TRUE
   )
   
+  compare_version <- utils::compareVersion(app_version, version_github)
   v_app_version <- glue("v{app_version}")
+  v_version_github <- glue("v{version_github}")
   
-  if(is.na(version_github))  return(HTML(i18n$t("You are running ACORN dashboard"), " ", v_app_version, " ", "<a href = https://github.com/acornamr/acorn-dashboard/releases/latest target='_blank' class = 'alert-link'>", i18n$t("You can check here if it's the latest production release.")))
+  if(is.na(version_github))  return(HTML(i18n$t("You are running ACORN dashboard"), " ", 
+                                         v_app_version, 
+                                         " ",
+                                         "<a href = https://github.com/acornamr/acorn-dashboard/releases/latest target='_blank' class = 'alert-link'>", 
+                                         i18n$t("You can check here if it's the latest production release.")))
   
-  if(version_github == v_app_version) return(HTML("<div class='alert alert-success'><i class='fa fa-check'></i>&nbsp;", 
-                                                                             i18n$t("Your ACORN dashboard is up to date"), " (", v_app_version, ")", "</div>"))
+  if(compare_version == 0 | compare_version == 1) return(HTML("<div class='alert alert-success'><i class='fa fa-check'></i>&nbsp;", 
+                                       i18n$t("Your ACORN dashboard is up to date"), " (", v_app_version, ")", "</div>"))
 
-  if(version_github != v_app_version) return(HTML("<div class='alert alert-warning'><i class='fa fa-exclamation-triangle'></i>&nbsp;", 
+  if(compare_version == -1) return(HTML("<div class='alert alert-warning'><i class='fa fa-exclamation-triangle'></i>&nbsp;", 
                                                                              i18n$t("You are running ACORN dashboard"),
                                                                              " ", v_app_version, " ",
                                                                              "<a href = https://github.com/acornamr/acorn-dashboard/releases/latest target='_blank' class = 'alert-link'>",
                                                                              i18n$t("Get the latest production release"), 
-                                                                             " (", version_github, ").", "</a></div>"))
+                                                                             " (", v_version_github, ").", "</a></div>"))
 })
