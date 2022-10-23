48b2566c753c0fd81008479c1a7347d6e07394ac
Olivier <olivier.celhay@gmail.com>
2022-10-23 10:20:27 -0700

    feat: add "No organism name / culture result" bar

--- inst/acorn/www/R/outputs/tab_microbiology/culture_specgroup.R
+++ inst/acorn/www/R/outputs/tab_microbiology/culture_specgroup.R
@@ -17,23 +17,29 @@ output$culture_specgroup <- renderHighchart({
   dta2 <- dta %>%
     fun_deduplication(method = input$deduplication_method) %>%
     mutate(growth = case_when(specid %in% spec_grown ~ "Growth", TRUE ~ "No Growth")) %>%
-    mutate(culture_result = case_when(orgname == "Not cultured" ~ "Not cultured", TRUE ~ growth)) %>%
+    mutate(
+      culture_result = case_when(
+        orgname == "Not cultured" ~ "Not cultured",
+        orgname == "No organism name / culture result" ~ "No organism name / culture result",
+        TRUE ~ growth
+      )
+    ) %>%
     group_by(specgroup, culture_result) %>%
     summarise(n = n_distinct(specid), .groups = "drop") %>%
     complete(specgroup, culture_result, fill = list(n = 0))
   
   dta2 <- left_join(dta2,
                     dta2 %>%
-                     group_by(specgroup) %>%
-                     summarise(total = sum(n), .groups = "drop"),
-                   by = "specgroup") %>%
+                      group_by(specgroup) %>%
+                      summarise(total = sum(n), .groups = "drop"),
+                    by = "specgroup") %>%
     mutate(freq = 100*round(n/total, 2)) %>%
     arrange(desc(total))
   
   dta2 %>%
     hchart(type = "bar", hcaes(x = "specgroup", y = "n", group = "culture_result")) %>%
     hc_xAxis(title = "") %>% hc_yAxis(title = "") %>%
-    hc_colors(c("#8e44ad", "#7f8c8d", "#d35400")) %>%
+    hc_colors(c("#8e44ad", "#7f8c8d", "#fef0d9", "#fc8d59")) %>%
     hc_plotOptions(bar = list(stacking = "normal")) %>%
     hc_tooltip(pointFormat = "{point.culture_result}: {point.y} specimens collected ({point.freq} %).") %>%
     hc_exporting(enabled = TRUE, buttons = list(contextButton = list(menuItems = hc_export_kind))) |> 
